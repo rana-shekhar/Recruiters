31b6f668ab3d475d25aa1a92f0a0edd1f0afa86f
rana-shekhar <shekharrana1011@gmail.com>
2024-12-02 17:36:57 +0530

    firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recruiters/splesh_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
